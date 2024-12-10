b4549d3bf06fe601bd118c0bb1a47690b508f343
Daniel Broboana <macbro@Daniels-MacBook-Air.local>
2024-12-10 23:32:02 +0200

    update method with retry decorator

--- etl/extractors/extractor_tradingview.py
+++ etl/extractors/extractor_tradingview.py
@@ -6,6 +6,7 @@ import networkx as nx
 from typing import List
 from pydantic import BaseModel
 from etl.utils.tradingview import TVHistoricalPrices
+from etl.utils.retry_decorator import retry
 from etl.trackers.perf_logging import performace_logging
 from etl.trackers.tracker_extractor import ExtractorTraker
 from etl.datasets.dataset import Dataset
@@ -26,21 +27,37 @@ class LastPriceTradingviewExtractor(ExtractorTraker):
         self._n_bar = 1
         self.data_source_label = config['data_source_label']
 
+    @retry(max_attempts=5, delay=1, backoff_factor=2)
+    def _get_data_for_symbol(self, symbol):
+        tv = TVHistoricalPrices(exchange=self._exchange)
+        try:
+            df = tv.get_hist(
+                symbol=symbol,
+                interval=self._interval, 
+                n_bars=self._n_bar
+            )
+            if df is None or df.empty:
+                raise ValueError(f"No data retrieved for symbol: {symbol}")
+            return df
+        
+        except Exception as e:
+            print(f'Error extracting data for {symbol}: {e}')
+            raise
+        finally:
+            del tv 
+    
+
     @performace_logging
-    def _extract_df(self, get_price_method):
-        dfs = [get_price_method(s) for s in self._symbols]
+    def _extract_df(self):
+        dfs = [self._get_data_for_symbol(s) for s in self._symbols]
         return pd.concat(dfs)
 
     def __call__(self):
-        tvph = TVHistoricalPrices(exchange=self._exchange)
-        get_price_method = functools.partial(
-                                tvph.get_hist, 
-                                interval=self._interval, 
-                                n_bars=self._n_bar)
-        df = self._extract_df(get_price_method)
+        df = self._extract_df()
         table = pa.Table.from_pandas(df)
         return Dataset(table, self.tracker_build_metadata(table))
-    
+
+
 
 
 
